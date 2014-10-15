4e8d5b4fcf53252e25052b86679f4ff84b6bfbaa
Ben Best <bbest@nceas.ucsb.edu>
2014-10-14 21:09:35 -0700

    moved israel/create n create_repo into create_all.R create_subcountry_regions.py now running on bumblebee thr

---
+++ create_all.R
@@ -0,0 +1,218 @@
+# create initial Baltic layers based on Global2013.www2013 layers
+# Requirements:
+#  1. spatial prep
+#  1. use RStudio to setup project
+#  1. install ohicore: http://ohi-science.org/pages/install.html
+#  1. clone of https://github.com/ohi-science/ohibaltic in ../ohicore
+
+library(stringr)
+
+# vars
+dir_data = 'N:/git-annex/clip-n-ship/data'
+
+# read in github token outside of repo, generated via https://help.github.com/articles/creating-an-access-token-for-command-line-use
+token = scan('~/.github-token', 'character')
+
+# get list of countries with prepped data
+cntries = list.files(dir_data)
+
+# loop through countries
+#for (cntry in cntries){
+cntry = 'Albania'  
+
+# repo name
+repo = sprintf('ohi-%s', tolower(str_replace_all(cntry,fixed(' '),'_')))
+
+# check for repo existence
+github_repo_exists = system(sprintf('git ls-remote git@github.com:ohi-science/%s.git', repo), ignore.stderr=T) != 128
+
+if (!github_repo_exists){
+  
+  # create using Github API: https://developer.github.com/v3/repos/#create
+  cmd = sprintf('curl -u "bbest:%s" https://api.github.com/orgs/ohi-science/repos -d \'{"name":"%s"}\'', token, repo)
+  system(cmd)
+  
+}
+
+# TODO: per repo
+# touch README.md
+# git init
+# git add README.md
+# git commit -m "first commit"
+# git remote add origin https://github.com/OHI-Science/ohi-albania.git
+# git push -u origin master
+
+
+setwd('~/github/ohi-israel/med2014')
+
+# get paths based on host machine
+dirs = list(
+  neptune_data  = '/Volumes/data_edit', 
+  neptune_local = '/Volumes/local_edit',
+  ohiprep       = '../../ohiprep',
+  ohicore       = '../../ohicore')
+
+# load ohicore, development mode
+library(devtools)
+load_all(dirs$ohicore)
+
+# other vars
+f_spatial    = '../../ohiprep/Israel/Hamaarag-Regions_v2014a/data/regions_gcs.js'
+dir_conf_in  = '../../ohi-global/eez2013/conf'
+dir_lyrs_in  = '../../ohi-global/eez2013/layers'
+
+
+# create dirs
+for (dir in c('tmp','layers','conf','spatial')) dir.create(dir, showWarnings=F)
+
+# load layers from 2013
+lyrs = read.csv('../../ohi-global/eez2013/layers.csv')
+write.csv(lyrs, 'tmp/layers_eez2013.csv', na='', row.names=F)
+
+# modify
+lyrs = lyrs %.%
+  select(
+    targets, layer, name, description, 
+    fld_value, units,
+    filename_old=filename) %.%
+  mutate(
+    filename = sprintf('%s_global2013.csv', layer)) %.%
+  arrange(targets, layer)
+write.csv(lyrs, 'tmp/layers.csv', na='', row.names=F)
+
+# csvs for regions and countries
+rgn_new_csv   = '../ohiprep/Israel/Hamaarag-Regions_v2014a/data/rgn_offshore_data.csv'
+rgn_old_csv   = sprintf('%s/rgn_labels.csv', dir_lyrs_in)
+cntry_old_csv = sprintf('%s/cntry_rgn.csv', dir_lyrs_in)
+
+# old to new regions
+rgn_new = read.csv(rgn_new_csv) %.%
+  select(rgn_id_new=rgn_id, rgn_name_new=rgn_name) %.%
+  mutate(rgn_name_old = 'Israel') %.%
+  merge(
+    read.csv(rgn_old_csv, na.strings='') %.%
+      select(rgn_name_old=label, rgn_id_old=rgn_id),
+    by='rgn_name_old', all.x=T) %.%
+  select(rgn_id_new, rgn_name_new, rgn_id_old, rgn_name_old) %.%
+  arrange(rgn_name_new)
+
+# old to new countries
+cntry_new = read.csv(cntry_old_csv) %.%
+  select(cntry_key, rgn_id_old=rgn_id) %.%
+  merge(
+    rgn_new,
+    by='rgn_id_old') %.%
+  group_by(cntry_key, rgn_id_new) %.%
+  summarise(n=n()) %.%
+  select(cntry_key, rgn_id_new) %.%
+  as.data.frame()
+
+for (i in 1:nrow(lyrs)){ # i=1
+  csv_in  = sprintf('%s/%s', dir_lyrs_in, lyrs$filename_old[i])
+  csv_out = sprintf('layers/%s', lyrs$filename[i])
+  
+  d = read.csv(csv_in, na.strings='')
+  flds = names(d)
+  
+  if ('rgn_id' %in% names(d)){
+    d = d %.%
+      filter(rgn_id %in% rgn_new$rgn_id_old) %.%
+      merge(rgn_new, by.x='rgn_id', by.y='rgn_id_old') %.%
+      mutate(rgn_id=rgn_id_new) %.%
+      subset(select=flds)
+  }
+  if ('cntry_key' %in% names(d)){
+    d = d %.%
+      filter(cntry_key %in% cntry_new$cntry_key)
+  }
+    
+  if (lyrs$layer[i]=='rgn_labels'){
+    csv_out = sprintf('layers/%s_israel2014.csv', lyrs$layer[i])
+    lyrs$filename[i] = basename(csv_out)
+    d = d %.%
+      merge(rgn_new, by.x='rgn_id', by.y='rgn_id_new') %.%
+      select(rgn_id, type, label=rgn_name_new)
+  }
+  write.csv(d, csv_out, row.names=F, na='')
+  
+  if (nrow(d)==0) {
+    dir.create('tmp/layers_empty', showWarnings=F)
+    file.copy(csv_in, file.path('tmp/layers_empty', lyrs$filename[i]))
+  }  
+}
+
+# copy custom layers
+for (f in list.files('tmp/layers_custom', full.names=T)){ # f = list.files('tmp/layers_custom', full.names=T)[1]
+  file.copy(f, file.path('layers', basename(f)), overwrite=T)  
+}
+
+# layers registry
+write.csv(select(lyrs, -filename_old), 'layers.csv', row.names=F, na='')
+
+# run checks on layers
+CheckLayers('layers.csv', 'layers', 
+            flds_id=c('rgn_id','cntry_key','country_id','saup_id','fao_id','fao_saup_id'))
+
+# order for layers for substitution old to new name in files
+lyrs = lyrs %.%
+  arrange(desc(nchar(as.character(layer_old))))
+
+# copy configuration files
+conf_files = c('config.R','functions.R','goals.csv','pressures_matrix.csv','resilience_matrix.csv','resilience_weights.csv')
+for (f in conf_files){ # f = conf_files[1]
+  
+  f_in  = sprintf('%s/%s', dir_conf_in, f)
+  f_out = sprintf('conf/%s', f)
+  
+  # read in file
+  s = readLines(f_in, warn=F, encoding='UTF-8')
+  
+#   # substitute old layer names with new
+#   for (i in 1:nrow(lyrs)){ # i=1
+#     s = gsub(lyrs$layer_old[i], lyrs$layer[i], s, fixed=T)
+#   }
+#   writeLines(s, f_out)
+  
+  # update confugration
+  if (f=='config.R'){
+    # set map center and zoom level
+    s = gsub('map_lat=0; map_lon=0; map_zoom=3', 'map_lat=32.5; map_lon=34.5; map_zoom=8', s) # Israel specific
+    # use just rgn_labels (not rgn_global)
+    s = gsub('rgn_global', 'rgn_labels', s)
+  }
+  
+  writeLines(s, f_out)
+}
+
+# append custom functions to overwrite others
+cat(
+  paste(c(
+    '\n\n\n# CUSTOM FUNCTIONS ----\n',
+    readLines('tmp/functions_custom.R', warn=F, encoding='UTF-8')), 
+    collapse='\n'), 
+  file='conf/functions.R', append=T)
+    
+# DEBUG: copy custom layers
+for (f in list.files('tmp/layers_custom', full.names=T)){ # f = list.files('tmp/layers_custom', full.names=T)[1]
+  file.copy(f, file.path('layers', basename(f)), overwrite=T)  
+}
+
+# calculate scores
+layers = Layers('layers.csv', 'layers')
+conf   = Conf('conf') # load_all(dirs$ohicore)
+scores = CalculateAll(conf, layers, debug=T)
+write.csv(scores, 'scores.csv', na='', row.names=F)
+
+# spatial
+for (f in f_spatial){ # f = f_spatial[1]
+  file.copy(f, sprintf('spatial/%s', basename(f)), overwrite=T)
+}
+ 
+# save shortcut files not specific to operating system
+write_shortcuts('.', os_files=0)
+
+# launch on Mac
+system('open launch_app.command')
+
+
+#} # end for (cntry in cntries)
--- create_subcountry_regions.py
+++ create_subcountry_regions.py
@@ -1,6 +1,7 @@
-# Run on cmd: C:\Python27\ArcGISx6410.2\python.exe G:\ohi-webapps\create_subcountry_regions.py
-
+# Run on cmd: C:\Python27\ArcGISx6410.2\python.exe G:\ohi-webapps\create_subcountry_regions.py # salacia
+#             C:\Python27\ArcGIS10.2\python.exe C:\Users\visitor\Documents\github\ohi-webapps\create_subcountry_regions.py # bumblebee in NCEAS Viz Lab
 # packages
+
 import arcpy, os, socket, numpy, numpy.lib.recfunctions, pandas, time, re, shutil
 
 # paths on NCEAS vis lab machine BUMBLEBEE (and # salacia - BB's Vmware WinXP)
@@ -26,7 +27,6 @@ sr_gcs = arcpy.SpatialReference('WGS 1984')          # geographic coordinate sys
 # environment
 os.chdir(wd)
 if not os.path.exists('tmp'):  os.makedirs('tmp')
-#if not os.path.exists('data'): os.makedirs('data')
 if not os.path.exists(dir_tmp):  os.makedirs(dir_tmp)
 if not arcpy.Exists(gdb):      arcpy.CreateFileGDB_management(os.path.dirname(gdb), os.path.basename(gdb))
 arcpy.env.overwriteOutput        = True
@@ -37,18 +37,23 @@ arcpy.env.outputCoordinateSystem = sr_gcs
 shps_rgn = ['rgn_gcs'] + ['rgn_%s_gcs' % b for b in buffers]
 for fc_rgn in [fc_gadm] + ['%s\\%s.shp' % (dir_rgn, x) for x in shps_rgn]:
 	fc_gdb = os.path.splitext(os.path.basename(fc_rgn))[0]
-	if not arcpy.Exists(fc_gdb):		
+	if not arcpy.Exists(fc_gdb):
+		print 'copying', fc_gdb
 		arcpy.CopyFeatures_management(fc_rgn, fc_gdb)
 
 	# convert gcs to mol
 	fc_mol = fc_gdb.replace('_gcs', '_mol')
 	if not arcpy.Exists(fc_mol):
+		print 'projecting', fc_mol
 		arcpy.Project_management(fc_gdb, fc_mol, sr_mol)
 		
 # get admin level 1 (sub country) spatial units		
-arcpy.Dissolve_management('gadm2', 'gadm2_admin1', ['NAME_0','NAME_1'])
+if not arcpy.Exists('gadm2_admin1'):
+	print 'dissolving gadm2 to gadm2_admin1'
+	arcpy.Dissolve_management('gadm2', 'gadm2_admin1', ['NAME_0','NAME_1'])
 		
 # get list of rgn countries
+print 'getting lists of countries rgn_gcs vs gadm2'
 df_rgn = pandas.DataFrame(arcpy.da.TableToNumPyArray(
     'rgn_gcs',
     ['OBJECTID','rgn_id','rgn_name'],
@@ -83,128 +88,136 @@ df_rgn = df_rgn[~df_rgn.rgn_name.isin(rgn_skip)]
 df_rgn.to_csv('tmp/rgn_ok_gadm.csv', index=False, encoding='utf-8')
 
 # iterate over regions
-##for rgn in sorted(tuple(df_rgn['rgn_name']))[1:6]: 
-#DEBUG
-rgn = sorted(tuple(df_rgn['rgn_name']))[0]
+print 'looping over countries'
+for rgn in sorted(tuple(df_rgn['rgn_name'])): # rgn = sorted(tuple(df_rgn['rgn_name']))[2]
 
-# make output dir
-print rgn
-dir_tmp_rgn = '%s/data/%s' % (dir_tmp, rgn.replace(' ', '_'))
-dir_dest_rgn = '%s/data/%s' % (dir_dest, rgn.replace(' ', '_'))
-if not os.path.exists(dir_tmp_rgn): os.makedirs(dir_tmp_rgn)
-if not os.path.exists(dir_dest_rgn): os.makedirs(dir_dest_rgn)
+	# make output dir
+	print rgn
+	n = int(arcpy.GetCount_management('c_eezland').getOutput(0))
 
-# select rgn to country only, now in Mollweide projection
-arcpy.env.outputCoordinateSystem = sr_mol
-arcpy.Select_analysis('rgn_gcs'     , 'c_eezland',                       "rgn_name = '%s'" % rgn)
-arcpy.Select_analysis('rgn_gcs'     , 'c_eez'    , "rgn_type = 'eez'  AND rgn_name = '%s'" % rgn)
-arcpy.Select_analysis('rgn_gcs'     , 'c_land'   , "rgn_type = 'land' AND rgn_name = '%s'" % rgn)
-arcpy.Select_analysis('gadm2_admin1', 'c_gadm'   , "NAME_0 = '%s'" % rgn)
+	dir_tmp_rgn = '%s/data/%s' % (dir_tmp, rgn.replace(' ', '_'))
+	dir_dest_rgn = '%s/data/%s' % (dir_dest, rgn.replace(' ', '_'))
+	if not os.path.exists(dir_tmp_rgn): os.makedirs(dir_tmp_rgn)
+	if not os.path.exists(dir_dest_rgn): os.makedirs(dir_dest_rgn)
 
-# remove fields which are from global analysis, not to be confused with subcountry fields
-for fld in ['rgn_type','rgn_id','rgn_name','rgn_key','area_km2']:
-    for fc in ['c_eez','c_land','c_eezland']:
-		if fld in [x.name for x in arcpy.ListFields(fc)]:
-			arcpy.DeleteField_management(fc, fld)
+	# delete existing country specific feature classes
+	for fc in arcpy.ListFeatureClasses('c_*') + arcpy.ListFeatureClasses('lyr_*'):
+		arcpy.Delete_management(fc)
+		
+	# select rgn to country only, now in Mollweide projection
+	arcpy.env.outputCoordinateSystem = sr_mol
+	rgn_name = 'American Samoa'
+	arcpy.Select_analysis('rgn_gcs'     , 'c_eezland',                       "rgn_name = '%s'" % rgn)
+	arcpy.Select_analysis('rgn_gcs'     , 'c_eez'    , "rgn_type = 'eez'  AND rgn_name = '%s'" % rgn)
+	arcpy.Select_analysis('rgn_gcs'     , 'c_land'   , "rgn_type = 'land' AND rgn_name = '%s'" % rgn)
+	arcpy.Select_analysis('gadm2_admin1', 'c_gadm'   , "NAME_0 = '%s'" % rgn)
 
-# get administrative land
-arcpy.Clip_analysis('c_gadm', 'c_land', 'c_states')
- 
-# create theissen polygons used to split slivers
-arcpy.Densify_edit('c_states', 'DISTANCE', '1 Kilometers')
-arcpy.FeatureVerticesToPoints_management('c_states', 'c_states_pts', 'ALL')
- 
-# delete interior points for faster thiessen rendering
-arcpy.Dissolve_management('c_states', 'c_states_d')
-arcpy.MakeFeatureLayer_management('c_states_pts', 'lyr_c_states_pts')
-arcpy.SelectLayerByLocation_management('lyr_c_states_pts', 'WITHIN_CLEMENTINI', 'c_states_d')
-arcpy.DeleteFeatures_management('lyr_c_states_pts')
- 
-# generate thiessen polygons of gadm for intersecting with land slivers
-arcpy.env.extent = 'c_eezland'
-arcpy.CreateThiessenPolygons_analysis('c_states_pts', 'c_states_t', 'ALL')
-arcpy.Dissolve_management('c_states_t', 'c_states_t_d', 'NAME_1')
-arcpy.RepairGeometry_management('c_states_t_d')
+	# remove fields which are from global analysis, not to be confused with subcountry fields
+	for fld in ['rgn_type','rgn_id','rgn_name','rgn_key','area_km2']:
+		for fc in ['c_eez','c_land','c_eezland']:
+			if fld in [x.name for x in arcpy.ListFields(fc)]:
+				arcpy.DeleteField_management(fc, fld)
 
-# add detailed interior back
-arcpy.Erase_analysis('c_states_t_d', 'c_states', 'c_states_t_d_e')
-arcpy.Merge_management(['c_states', 'c_states_t_d_e'], 'c_states_t_d_e_m')
-arcpy.Dissolve_management('c_states_t_d_e_m', 'c_thiessen', 'NAME_1')
- 
-# rgn_offshore: rename NAME_1 to rgn_name
-print 'rgn_offshore...'
-arcpy.Intersect_analysis(['c_eez', 'c_thiessen'], 'c_eez_t', 'NO_FID')
-arcpy.Dissolve_management('c_eez_t', 'c_rgn_offshore_mol', 'NAME_1')
-arcpy.AddField_management('c_rgn_offshore_mol', 'rgn_name', 'TEXT')
-arcpy.CalculateField_management('c_rgn_offshore_mol', 'rgn_name', '!NAME_1!', 'PYTHON_9.3')
-arcpy.DeleteField_management('c_rgn_offshore_mol', 'NAME_1')
- 
-# rgn_offshore: assign rgn_id by ascending y coordinate
-arcpy.AddField_management('c_rgn_offshore_mol', 'centroid_y', 'FLOAT')
-arcpy.CalculateField_management('c_rgn_offshore_mol', 'centroid_y', '!shape.centroid.y!', 'PYTHON_9.3')
-a = arcpy.da.TableToNumPyArray('c_rgn_offshore_mol', ['centroid_y','rgn_name'])
-a.sort(order=['centroid_y'], axis=0)
-a = numpy.lib.recfunctions.append_fields(a, 'rgn_id', range(1, a.size+1), usemask=False)
-arcpy.da.ExtendTable('c_rgn_offshore_mol', 'rgn_name', a[['rgn_name','rgn_id']], 'rgn_name', append_only=False)
-arcpy.DeleteField_management('c_rgn_offshore_mol', 'centroid_y')
+	# get administrative land
+	arcpy.Clip_analysis('c_gadm', 'c_land', 'c_states')
+	 
+	# create theissen polygons used to split slivers
+	arcpy.Densify_edit('c_states', 'DISTANCE', '1 Kilometers')
+	arcpy.FeatureVerticesToPoints_management('c_states', 'c_states_pts', 'ALL')
+	 
+	# delete interior points for faster thiessen rendering
+	arcpy.Dissolve_management('c_states', 'c_states_d')
+	arcpy.MakeFeatureLayer_management('c_states_pts', 'lyr_c_states_pts')
+	arcpy.SelectLayerByLocation_management('lyr_c_states_pts', 'WITHIN_CLEMENTINI', 'c_states_d')
+	arcpy.DeleteFeatures_management('lyr_c_states_pts')
+	 
+	# generate thiessen polygons of gadm for intersecting with land slivers
+	arcpy.env.extent = 'c_eezland'
+	arcpy.CreateThiessenPolygons_analysis('c_states_pts', 'c_states_t', 'ALL')
+	arcpy.Dissolve_management('c_states_t', 'c_states_t_d', 'NAME_1')
+	arcpy.RepairGeometry_management('c_states_t_d')
 
-# rgn_inland
-print 'rgn_inland'
-arcpy.Intersect_analysis(['c_land', 'c_thiessen'], 'c_land_t', 'NO_FID')
-arcpy.Dissolve_management('c_land_t', 'c_rgn_inland_mol', 'NAME_1')
-arcpy.AddField_management('c_rgn_inland_mol', 'rgn_name', 'TEXT')
-arcpy.CalculateField_management('c_rgn_inland_mol', 'rgn_name', '!NAME_1!', 'PYTHON_9.3')
-arcpy.DeleteField_management('c_rgn_inland_mol', 'NAME_1')
-# rgn_inland: assign rgn_id
-arcpy.da.ExtendTable('c_rgn_inland_mol', 'rgn_name', a[['rgn_name','rgn_id']], 'rgn_name', append_only=False)
+	# add detailed interior back
+	arcpy.Erase_analysis('c_states_t_d', 'c_states', 'c_states_t_d_e')
+	arcpy.Merge_management(['c_states', 'c_states_t_d_e'], 'c_states_t_d_e_m')
+	arcpy.Dissolve_management('c_states_t_d_e_m', 'c_thiessen', 'NAME_1')
+	 
+	# rgn_offshore: rename NAME_1 to rgn_name
+	print '  rgn_offshore'
+	arcpy.Intersect_analysis(['c_eez', 'c_thiessen'], 'c_eez_t', 'NO_FID')
+	arcpy.Dissolve_management('c_eez_t', 'c_rgn_offshore_mol', 'NAME_1')
+	arcpy.AddField_management('c_rgn_offshore_mol', 'rgn_name', 'TEXT')
+	arcpy.CalculateField_management('c_rgn_offshore_mol', 'rgn_name', '!NAME_1!', 'PYTHON_9.3')
+	arcpy.DeleteField_management('c_rgn_offshore_mol', 'NAME_1')
+	 
+	# rgn_offshore: assign rgn_id by ascending y coordinate
+	arcpy.AddField_management('c_rgn_offshore_mol', 'centroid_y', 'FLOAT')
+	arcpy.CalculateField_management('c_rgn_offshore_mol', 'centroid_y', '!shape.centroid.y!', 'PYTHON_9.3')
+	a = arcpy.da.TableToNumPyArray('c_rgn_offshore_mol', ['centroid_y','rgn_name'])
+	a.sort(order=['centroid_y'], axis=0)
+	a = numpy.lib.recfunctions.append_fields(a, 'rgn_id', range(1, a.size+1), usemask=False)
+	arcpy.da.ExtendTable('c_rgn_offshore_mol', 'rgn_name', a[['rgn_name','rgn_id']], 'rgn_name', append_only=False)
+	arcpy.DeleteField_management('c_rgn_offshore_mol', 'centroid_y')
 
-# save
-arcpy.CopyFeatures_management('c_rgn_inland_mol', dir_tmp_rgn + '/rgn_inland_mol.shp')
-arcpy.CopyFeatures_management('c_rgn_offshore_mol', dir_tmp_rgn + '/rgn_offshore_mol.shp')
+	# rgn_inland
+	print '  rgn_inland'
+	arcpy.Intersect_analysis(['c_land', 'c_thiessen'], 'c_land_t', 'NO_FID')
+	arcpy.Dissolve_management('c_land_t', 'c_rgn_inland_mol', 'NAME_1')
+	arcpy.AddField_management('c_rgn_inland_mol', 'rgn_name', 'TEXT')
+	arcpy.CalculateField_management('c_rgn_inland_mol', 'rgn_name', '!NAME_1!', 'PYTHON_9.3')
+	arcpy.DeleteField_management('c_rgn_inland_mol', 'NAME_1')
+	# rgn_inland: assign rgn_id
+	arcpy.da.ExtendTable('c_rgn_inland_mol', 'rgn_name', a[['rgn_name','rgn_id']], 'rgn_name', append_only=False)
 
-# loop through buffers
-for buf in buffers: #buf = buffers[0]
+	# save
+	arcpy.CopyFeatures_management('c_rgn_inland_mol', dir_tmp_rgn + '/rgn_inland_mol.shp')
+	arcpy.CopyFeatures_management('c_rgn_offshore_mol', dir_tmp_rgn + '/rgn_offshore_mol.shp')
 
-	rgn_buf_mol = '%s/rgn_%s_mol' % (gdb, buf)
-	buf_zone, buf_dist, buf_units = re.search('(\\D+)(\\d+)(\\D+)', buf).groups()    
-	print ' buffer: %s %s %s' % (buf_zone, buf_dist, buf_units)
+	# loop through buffers
+	for buf in buffers: #buf = buffers[0]
 
-	if buf_zone == 'inland':
-		arcpy.Intersect_analysis(['c_rgn_inland_mol', rgn_buf_mol], 'c_buf_t', 'NO_FID')
-	elif buf_zone == 'offshore':
-		arcpy.Intersect_analysis(['c_rgn_offshore_mol', rgn_buf_mol], 'c_buf_t', 'NO_FID')
-	else:
-		stop('The buf_zone "%s" is not handled by this function.' % buf_zone)
-	arcpy.Dissolve_management('c_buf_t', 'c_buf_t_d', 'rgn_name')
-	arcpy.da.ExtendTable('c_buf_t_d', 'rgn_name', a[['rgn_name','rgn_id']], 'rgn_name', append_only=False)
-	arcpy.CopyFeatures_management('c_buf_t_d', '%s/rgn_%s_mol.shp' % (dir_tmp_rgn, buf))
-    
-# project shapefiles to gcs, calculate area and export csv
-arcpy.env.workspace = dir_tmp_rgn
-arcpy.env.outputCoordinateSystem = sr_gcs
-for fc_mol in sorted(arcpy.ListFeatureClasses('rgn_*_mol.shp')):
-    print fc_mol
-    fc_gcs = fc_mol.replace('_mol', '_gcs')
-    csv = os.path.splitext(fc_gcs.replace('_gcs', ''))[0] + '_data.csv'
-    arcpy.Project_management(fc_mol, fc_gcs, sr_gcs)
-    arcpy.RepairGeometry_management(fc_gcs)
-    arcpy.AddField_management(fc_gcs, 'area_km2', 'FLOAT')
-    arcpy.CalculateField_management(fc_gcs, 'area_km2', '!shape.geodesicArea@squarekilometers!', 'PYTHON_9.3')
-    d = pandas.DataFrame(arcpy.da.TableToNumPyArray(fc_gcs, ['rgn_id','rgn_name','area_km2']))
-    d.to_csv('%s/rgn_%s_data.csv' % (dir_tmp_rgn, buf), index=False, encoding='utf-8')
+		rgn_buf_mol = '%s/rgn_%s_mol' % (gdb, buf)
+		buf_zone, buf_dist, buf_units = re.search('(\\D+)(\\d+)(\\D+)', buf).groups()    
+		print '    buffer: %s %s %s' % (buf_zone, buf_dist, buf_units)
 
-### Skipping simplify b/c at least for Albania trial decent PAEK tolerance of 0.01 actually producing larger *.shp vs original
-### simplify offshore to geojson for rendering in toolbox
-##arcpy.env.outputCoordinateSystem = sr_gcs
-##arcpy.RepairGeometry_management('rgn_offshore_gcs.shp')
-##arcpy.cartography.SmoothPolygon('rgn_offshore_gcs.shp', 'rgn_offshore_smooth_gcs.shp', 'PAEK', 0.01, 'FIXED_ENDPOINT', 'FLAG_ERRORS')
-##arcpy.env.outputCoordinateSystem = sr_mol # reset coordinate system
-##arcpy.cartography.SimplifyPolygon('rgn_offshore_gcs.shp', 'rgn_offshore_simplify_gcs.shp', 'BEND_SIMPLIFY', 1000, 0, 'FLAG_ERRORS', 'KEEP_COLLAPSED_POINTS')
+		if buf_zone == 'inland':
+			arcpy.Intersect_analysis(['c_rgn_inland_mol', rgn_buf_mol], 'c_buf_t', 'NO_FID')
+		elif buf_zone == 'offshore':
+			arcpy.Intersect_analysis(['c_rgn_offshore_mol', rgn_buf_mol], 'c_buf_t', 'NO_FID')
+		else:
+			stop('The buf_zone "%s" is not handled by this function.' % buf_zone)
+		arcpy.Dissolve_management('c_buf_t', 'c_buf_t_d', 'rgn_name')
+		arcpy.da.ExtendTable('c_buf_t_d', 'rgn_name', a[['rgn_name','rgn_id']], 'rgn_name', append_only=False)
+		arcpy.CopyFeatures_management('c_buf_t_d', '%s/rgn_%s_mol.shp' % (dir_tmp_rgn, buf))
+		
+	# project shapefiles to gcs, calculate area and export csv
+	arcpy.env.workspace = dir_tmp_rgn
+	arcpy.env.outputCoordinateSystem = sr_gcs
+	for fc_mol in sorted(arcpy.ListFeatureClasses('rgn_*_mol.shp')):
+		print '    project %s to gcs' % fc_mol
+		fc_gcs = fc_mol.replace('_mol', '_gcs')
+		csv = os.path.splitext(fc_gcs.replace('_gcs', ''))[0] + '_data.csv'
+		arcpy.Project_management(fc_mol, fc_gcs, sr_gcs)
+		arcpy.RepairGeometry_management(fc_gcs)
+		arcpy.AddField_management(fc_gcs, 'area_km2', 'FLOAT')
+		arcpy.CalculateField_management(fc_gcs, 'area_km2', '!shape.geodesicArea@squarekilometers!', 'PYTHON_9.3')
+		d = pandas.DataFrame(arcpy.da.TableToNumPyArray(fc_gcs, ['rgn_id','rgn_name','area_km2']))
+		d.to_csv('%s/rgn_%s_data.csv' % (dir_tmp_rgn, buf), index=False, encoding='utf-8')
 
-# reset workspace and coordinate system
-arcpy.env.outputCoordinateSystem = sr_gcs
-arcpy.env.workspace              = gdb
+	### Skipping simplify b/c at least for Albania trial decent PAEK tolerance of 0.01 actually producing larger *.shp vs original
+	### simplify offshore to geojson for rendering in toolbox
+	##arcpy.env.outputCoordinateSystem = sr_gcs
+	##arcpy.RepairGeometry_management('rgn_offshore_gcs.shp')
+	##arcpy.cartography.SmoothPolygon('rgn_offshore_gcs.shp', 'rgn_offshore_smooth_gcs.shp', 'PAEK', 0.01, 'FIXED_ENDPOINT', 'FLAG_ERRORS')
+	##arcpy.env.outputCoordinateSystem = sr_mol # reset coordinate system
+	##arcpy.cartography.SimplifyPolygon('rgn_offshore_gcs.shp', 'rgn_offshore_simplify_gcs.shp', 'BEND_SIMPLIFY', 1000, 0, 'FLAG_ERRORS', 'KEEP_COLLAPSED_POINTS')
 
-# copy to destination        
-shutil.rmtree(dir_dest_rgn) # empty it
-shutil.copytree(dir_tmp_rgn, dir_dest_rgn) # copy recursively
+	# reset workspace and coordinate system
+	arcpy.Compact_management(gdb)
+	arcpy.env.workspace              = gdb
+	arcpy.env.outputCoordinateSystem = sr_gcs
+
+	# copy to destination
+	print '  copy to destination'	
+	shutil.rmtree(dir_dest_rgn) # empty it
+	shutil.copytree(dir_tmp_rgn, dir_dest_rgn) # copy recursively
