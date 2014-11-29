800990701e5e82b595b8b5f037cabda405c39bca
Ben Best <bbest@nceas.ucsb.edu>
2014-11-28 23:21:55 -0800

    got last spatial subcountries working: rus, spm

--- fix_subcountry_buffers.py
+++ fix_subcountry_buffers.py
@@ -1,19 +1,47 @@
-import pandas, re
+import arcpy, pandas, re, os, fnmatch, time
 
-d = {'kor': ['offshore1km', 'inland1km']}
-gdb = r'C:\tmp\ohi-webapps\subcountry.gdb'
+# C:\Python27\ArcGISx6410.2\python.exe G:\ohi-webapps\fix_subcountry_buffers.py
+#
+# - are : create_maps: readOGR('/Volumes/data_edit/git-annex/clip-n-ship/are/spatial', 'rgn_inland1km_gcs') # Error in ogrInfo(dsn = dsn, layer = layer, encoding = encoding, use_iconv = use_iconv) : Multiple # dimensions:
+# - aus : create_maps: ggmap tile not found prob
+# - nic : missing spatial/rgn_offshore3nm_data.csv
+# - zaf : inland1km Error in ogrInfo(dsn = dsn, layer = layer, encoding = encoding, use_iconv = use_iconv) : Multiple # dimensions: 
+# TODO: zip shps: kor, are, zaf
+
+#d = {'esp': ['offshore1km', 'inland1km'],
+#     'kor': ['offshore1km', 'inland1km'],
+#     'zaf': ['offshore1km', 'inland1km']}
+#d = {'are': ['offshore1km', 'inland1km']}
+
+gdb     = 'C:/tmp/ohi-webapps/subcountry.gdb'
+dir_err = 'G:/ohi-webapps/errors'
+
+# set dictionary of subcountry keys and buffers that errored out before when running create_all
+##d = {}
+##for f in os.listdir(dir_err):
+##    if fnmatch.fnmatch(f, '*_readOGR_fails.txt'):
+##        key = f.replace('_readOGR_fails.txt','')
+##        d[key] = open('%s/%s' % (dir_err, f), 'r').readlines()[0].split(':')[1].strip().split(',')
+##k_todo = ['ury', 'vct', 'ven', 'vnm', 'vut', 'wlf', 'wsm', 'yem']
+##d = {k: d[k] for k in k_todo}
+##d = {'syr': ['inland1km'],
+##     'vnm': ['offshore3nm']}
+d = {'ukr': ['offshore3nm']}
+
 d_buf_units = {'nm':'NauticalMiles',
                'km':'Kilometers',
                'mi':'Miles'}
 
 def export_shp_csv(fc_in, fc_out, csv_out, flds=['rgn_id','rgn_name','area_km2']):
     arcpy.CopyFeatures_management(fc_in, fc_out)
-    d = pandas.DataFrame(arcpy.da.TableToNumPyArray(fc_in, flds))
-    d.to_csv(csv_out, index=False, encoding='utf-8')
+    # nic...: buf = 'offshore3nm'; fc_in = '%s/rgn_%s_gcs.shp' % (dir_sp, buf); csv_out = '%s/rgn_%s_data.csv' % (dir_sp, buf); flds=['rgn_id','rgn_name','area_km2']
+    dat = pandas.DataFrame(arcpy.da.TableToNumPyArray(fc_in, flds))
+    dat.to_csv(csv_out, index=False, encoding='utf-8')
 
-for key in d.iterkeys(): # key = 'esp'
+for key in sorted(d.iterkeys()): # key = 'esp'
 
     dir_sp = 'N:/git-annex/clip-n-ship/%s/spatial' % key
+    print '%s: %s [%s]' % (key, ', '.join(d[key]), time.strftime('%H:%M:%S'))
 
     # delete existing k_* in gdb
     arcpy.env.workspace = gdb
@@ -32,6 +60,7 @@ for key in d.iterkeys(): # key = 'esp'
     for buf in d[key]: # buf = 'inland1km' # buf = 'offshore1km'
     
         buf_zone, buf_dist, buf_units = re.search('(\\D+)(\\d+)(\\D+)', buf).groups()
+        print '  %s [%s]' % (buf, time.strftime('%H:%M:%S'))
 
         if buf_zone == 'inland':
             arcpy.Buffer_analysis('k_offshore_d', 'k_%s_b' % buf, '%s %s' % (buf_dist, d_buf_units[buf_units]), dissolve_option='ALL')
@@ -42,14 +71,15 @@ for key in d.iterkeys(): # key = 'esp'
             arcpy.Intersect_analysis(['k_%s_b' % buf, 'k_offshore'], 'k_%s_bi' % buf)
 
         arcpy.Dissolve_management('k_%s_bi' % buf, 'k_%s_bid' % buf, ['rgn_id','rgn_name'])
+        arcpy.RepairGeometry_management('k_%s_bid' % buf) # late add
         arcpy.AddField_management(      'k_%s_bid' % buf, 'area_km2', 'DOUBLE')
         arcpy.CalculateField_management('k_%s_bid' % buf, 'area_km2', '!shape.area@SQUAREKILOMETERS!', 'PYTHON_9.3')
 
         # delete existing shp, tif
         arcpy.env.workspace = dir_sp
-        d = arcpy.ListFeatureClasses('*%s*' % buf)
-        d.extend(arcpy.ListDatasets('*%s*' % buf))
-        for o in d:
+        objects = arcpy.ListFeatureClasses('*%s*' % buf)
+        objects.extend(arcpy.ListDatasets('*%s*' % buf))
+        for o in objects:
             arcpy.Delete_management(o)
 
         # export shp, csv
