9b9f3fb716edf76f7b7aa4348085f6b9cd77f095
jules32 <jstewart@nceas.ucsb.edu>
2015-03-20 17:53:38 -0700

    add deploy_app_nceas call

--- edit_webapps.R
+++ edit_webapps.R
@@ -12,3 +12,12 @@ sapply(keys, update_website, 'update About using ohi-webapps/create_functions.R 
 
 # TODO: fix update_draft function to copy functions.R and update layers.csv descriptions.
 #sapply(keys, update_draft, 'update About using ohi-webapps/create_functions.R - update_draft()') 
+
+
+# Move Apps to NCEAS server
+# change app_url to https://ohi-science.nceas.ucsb.edu/ (from https://ohi-science.shinyapps.io) in: 
+#   1. ohi-webapps/gh-pages/_config.brew.yml
+#   2. ohi-webapps/app.brew.yml
+keys = sc_studies %>% filter(!is.na(sc_annex_dir)) %>% select(sc_key)
+keys = keys[,1] # keys = 'blz'
+# sapply(keys, deploy_app_nceas, 'move Shiny App onto NCEAS server - deploy_app_nceas()')
