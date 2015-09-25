4d714ab6b4fe6c847970323404b4c5ae00e9b18d
jules32 <jstewart@nceas.ucsb.edu>
2015-09-25 09:59:33 -0700

    created cleangeo_spatial_fxn.r

---
+++ custom/cleangeo_spatial_fxn.R
@@ -0,0 +1,32 @@
+# cleangeo_spatial.r
+# J. Lowndes @jules32, August 2015
+# check for and clean any 'orphaned holes': http://gis.stackexchange.com/questions/113964/fixing-orphaned-holes-in-r
+# ------------------
+
+cleangeo_spatial = function(sp_data) {
+  
+  # identify any issues in spatial sp_data ----
+  library(rgdal)
+  library(raster)
+  library(cleangeo) # devtools::install_github('eblondel/cleangeo')  # https://github.com/eblondel/cleangeo
+  
+  cat('checking for orphan holes or invalid geometries...')
+  
+  #get a report of geometry validity & issues for a spatial object
+  report = clgeo_CollectionReport(sp_data)
+  summary = clgeo_SummaryReport(report)
+  issues = report[report$valid == FALSE,]
+  cat(sprintf('these are the issues pre-clean: \n %s \n\n', issues %>% select(warning_msg)))
+  
+  # fix any issues in spatial sp_data ----
+  cat('fixing any orphan holes or invalid geometries...')
+  
+  sp_data_tmp = sp_data
+  sp_data_clean = clgeo_Clean(sp_data_tmp, print.log=T) # mybhi.clean_archive = mybhi.clean # save a copy
+  report_clean = clgeo_CollectionReport(sp_data_clean)
+  summary_clean = clgeo_SummaryReport(report_clean)
+  issues = report_clean[report_clean$valid == FALSE,]
+  cat(sprintf('these are the issues post-clean: \n %s \n\n', issues %>% select(warning_msg)))
+  
+  return(sp_data_clean)
+} 
