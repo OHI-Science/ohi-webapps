8784d4baa880c5a6fbd7aca8ee838d84b99f566a
jules32 <jstewart@nceas.ucsb.edu>
2016-05-27 13:27:04 -0700

    archived regions_gcs_*.geojson from all OHI+ repos prior to running delete_unused_repos.r

--- OHI_EEZGADM_download.R
+++ OHI_EEZGADM_download.R
@@ -1,63 +1,110 @@
-#CODE TO DOWNLOAD GEOJSON FILES FROM OHI GITHUB ARCHIVE, CONVERT TO ESRI SHAPEFILES
-#AND SAVE TO DESTINATION FOLDER
-#
-#WRITTEN BY CHARLOTTE MARCINKO 08-04-2016
-#UNIVERSITY OF SOUTHAMPTON AND NATIONAL OCEANOGRAPHY CENTRE UK 
-#
-#######################################################################################
+## OHI_EEZGADM_download.r
+## download OHI GADM boundaries and save
+## based on code below by Charlotte Marcinko; archived at bottom. 
 
-#HOUSE KEEPING
-
+## setup ----
 rm(list = ls())
 
+source('create_init.r')
 library(sp) 
 library(rgdal)
 library(utils)
 
-#READ IN LIST OF COUNTRY CODES 
-#testcodes<-c("aus", "bra","can","ind","usa","rus")
+log_not_downloaded = data_frame()
 
-codes.data<-read.csv("D:\\GULLS\\DataIn\\OHI_EEZ_gadm\\Country_codes_ohi.csv",sep=",")
-codes<-codes.data$repo
-
+## for each sc_key
+for (key in sc_studies$sc_key) { # key = "bra"
   
-#LOOP THROUGH COUNTRY CODE 
-  for (ii in 1:length(codes)) {
+  ## trycatch to skip to next sc_key if data is missing from archive
+  possibleError<-tryCatch({
     
-#INCLUDE TRYCATCH TO SKIP TO NEXT REPO CODE IF DATA MISSING FROM ARCHIVE
-    possibleError<-tryCatch({
-      
-#SET DESTINATION FILE FOR DOWNLOAD
+    ## set destination file to save downloaded file
+    file_save <- path.expand(paste0('~/github/ohi-webapps/ohi_gadm_geojson_archive/regions_gcs_', key , '.geojson'))
+    
+    ## set url for to download file
+    file_url <- paste("https://raw.githubusercontent.com/OHI-Science",
+                      key, "draft/subcountry2014/spatial/regions_gcs.geojson", sep="/")
+    
+    ## download geojson file for key
+    download.file(file_url, file_save, method = "curl")
+    
+  },error=function(e) e) # end trycatch
   
-      destfile<-paste("C:\\Users\\grw\\GULLS\\OHI_EEZ_GADM\\regions_gcs_",codes[ii],".geojson",sep="")  
- 
-#SET URL FOR REQUIRED DATA
   
-      url<-paste("https://raw.githubusercontent.com/OHI-Science",codes[ii],"draft/subcountry2014/spatial/regions_gcs.geojson",sep="/")
- 
-#DOWNLOAD OHI GEOJSON FILE FOR COUNTRY ii
-      download.file(url, destfile, method = "wininet")
-
-#READ IN COUNTRY GEOJSON FILE ii  
-      x = readOGR(dsn = destfile,"OGRGeoJSON")
-  
-#SET DESTINATION PATH FOR SHAPEFILE AND LAYER NAME
-      ESRIpath<-paste("C:\\Users\\grw\\GULLS\\OHI_EEZ_GADM\\regions_gcs_",codes[ii],sep="")  
-      layerName<-paste(codes[ii],"_EEZGADM1",sep="")
-
-#WRITE COUNTRY ii FILE AS ESRI SHAPEFILE  
-      writeOGR(x, dsn = ESRIpath,
-             layer = layerName, driver = 'ESRI Shapefile', overwrite=T)
-
-# REMOVE X FOR NEXT ITERATION 
-      rm(x,ESRIpath,layerName,url,destfile)
-
+  ## if error in download skip to next but record
+  if(inherits(possibleError, "error")) {
    
-},error=function(e) e) # END OF TRYCATCH 
-    
-#IF ERROR IN DATA AQUISITION THEN SKIP TO NEXT ii    
-    
-    if(inherits(possibleError, "error")) next
-    
-  } #END LOOP 
+    log_not_downloaded = log_not_downloaded %>%
+      bind_rows(data_frame(key))
+     
+    next
+  }
+  
+} 
 
+
+# 
+# 
+# #CODE TO DOWNLOAD GEOJSON FILES FROM OHI GITHUB ARCHIVE, CONVERT TO ESRI SHAPEFILES
+# #AND SAVE TO DESTINATION FOLDER
+# #
+# #WRITTEN BY CHARLOTTE MARCINKO 08-04-2016
+# #UNIVERSITY OF SOUTHAMPTON AND NATIONAL OCEANOGRAPHY CENTRE UK 
+# #
+# #######################################################################################
+# 
+# #HOUSE KEEPING
+# 
+# rm(list = ls())
+# 
+# library(sp) 
+# library(rgdal)
+# library(utils)
+# 
+# #READ IN LIST OF COUNTRY CODES 
+# #testcodes<-c("aus", "bra","can","ind","usa","rus")
+# 
+# codes.data<-read.csv("D:\\GULLS\\DataIn\\OHI_EEZ_gadm\\Country_codes_ohi.csv",sep=",")
+# codes<-codes.data$repo
+# 
+#   
+# #LOOP THROUGH COUNTRY CODE 
+#   for (ii in 1:length(codes)) {
+#     
+# #INCLUDE TRYCATCH TO SKIP TO NEXT REPO CODE IF DATA MISSING FROM ARCHIVE
+#     possibleError<-tryCatch({
+#       
+# #SET DESTINATION FILE FOR DOWNLOAD
+#   
+#       destfile<-paste("C:\\Users\\grw\\GULLS\\OHI_EEZ_GADM\\regions_gcs_",codes[ii],".geojson",sep="")  
+#  
+# #SET URL FOR REQUIRED DATA
+#   
+#       url<-paste("https://raw.githubusercontent.com/OHI-Science",codes[ii],"draft/subcountry2014/spatial/regions_gcs.geojson",sep="/")
+#  
+# #DOWNLOAD OHI GEOJSON FILE FOR COUNTRY ii
+#       download.file(url, destfile, method = "wininet")
+# 
+# #READ IN COUNTRY GEOJSON FILE ii  
+#       x = readOGR(dsn = destfile,"OGRGeoJSON")
+#   
+# #SET DESTINATION PATH FOR SHAPEFILE AND LAYER NAME
+#       ESRIpath<-paste("C:\\Users\\grw\\GULLS\\OHI_EEZ_GADM\\regions_gcs_",codes[ii],sep="")  
+#       layerName<-paste(codes[ii],"_EEZGADM1",sep="")
+# 
+# #WRITE COUNTRY ii FILE AS ESRI SHAPEFILE  
+#       writeOGR(x, dsn = ESRIpath,
+#              layer = layerName, driver = 'ESRI Shapefile', overwrite=T)
+# 
+# # REMOVE X FOR NEXT ITERATION 
+#       rm(x,ESRIpath,layerName,url,destfile)
+# 
+#    
+# },error=function(e) e) # END OF TRYCATCH 
+#     
+# #IF ERROR IN DATA AQUISITION THEN SKIP TO NEXT ii    
+#     
+#     if(inherits(possibleError, "error")) next
+#     
+#   } #END LOOP 
+# 
---
+++ ohi_gadm_geojson_archive/README.md
@@ -0,0 +1,6 @@
+# README
+
+These .geojson files were downloaded from all ohi-science repos using `ohi-webapps/OHI_EEZGADM_download.r` on May 27 2016 before deleting many of the unused OHI+ repos with `ohi-webapps/delete_unused_repos.r`.
+
+Each of the `regions_gcs_*.geojson` files were made by @bbest: Exclusive economic zones (EEZs) were identified by MarineRegions.org and the largest subcountry regions were identified by the generalized administrative boundary database GADM.org. Region boundaries were extended offshore to divide the EEZ of Baltic into offshore regions.
+
