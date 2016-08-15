ff8770572935ad7a48ed1f164abfb915f74d5b6a
jules32 <jstewart@nceas.ucsb.edu>
2016-08-15 14:44:14 -0700

    cleaning up filepath calls; put goals.rmd in inst/

--- R/populate_conf.R
+++ R/populate_conf.R
@@ -1,9 +1,9 @@
 # populate_conf.r
 
-populate_conf <- function(key=key) {
+populate_conf <- function(key, dir_scenario, dir_global) {
   
   ## create conf folder
-  dir.create(file.path(dir_repo, default_scenario, 'conf'))
+  dir.create(file.path(dir_scenario, 'conf'))
   
   ## list conf files to copy
   conf_files = c('config.R','functions.R','goals.csv',
@@ -12,35 +12,11 @@ populate_conf <- function(key=key) {
   
   for (f in conf_files){ # f = conf_files[1]
     
-    f_in  = sprintf('%s/conf/%s', dir_global, f)
-    f_out = sprintf('conf/%s', f)
+    f_in  = sprintf('%s/conf/%s', dir_global,   f)
+    f_out = sprintf('%s/conf/%s', dir_scenario, f)
     
     # read in file
     s = readLines(f_in, warn=F, encoding='UTF-8')
-    
-    # ## update confugration
-    # if (f=='config.R'){
-    #   
-    #   ## get map centroid and zoom level
-    #   # TODO: save this to separate map file...
-    #   # TODO: would be great to set this info when making maps so center is reset if map changes, not sure that's feasible...
-    #   # TODO: http://gis.stackexchange.com/questions/76113/dynamically-set-zoom-level-based-on-a-bounding-box
-    #   # var regions_group = new L.featureGroup(regions); map.fitBounds(regions_group.getBounds());
-    #   p_shp  = file.path(dir_annex_sc, 'spatial', 'rgn_offshore_gcs.shp')
-    #   p      = rgdal::readOGR(dirname(p_shp), tools::file_path_sans_ext(basename(p_shp)))
-    #   p_bb   = data.frame(p@bbox) # max of 2.25
-    #   p_ctr  = rowMeans(p_bb)
-    #   p_zoom = 12 - as.integer(cut(max(transmute(p_bb, range = max - min)), 
-    #                                c(0, 0.25, 0.5, 1, 2.5, 5, 10, 20, 40, 80, 160, 320, 360)))
-    #   
-    #   ## set map center and zoom level
-    #   s = s %>%
-    #     str_replace("map_lat.*", sprintf('map_lat=%g; map_lon=%g; map_zoom=%d', 
-    #                                      p_ctr['y'], p_ctr['x'], p_zoom)) # updated JSL to overwrite any map info
-    #   
-    #   ## use just rgn_labels (not rgn_global)
-    #   s = gsub('rgn_global', 'rgn_labels', s)
-    # } 
     
     ## swap out custom functions
     if (f=='functions.R'){
@@ -87,15 +63,16 @@ populate_conf <- function(key=key) {
   } # end for (f in conf_files)
   
   ## swap fields in goals.csv
-  goals = read.csv('conf/goals.csv', stringsAsFactors=F)
+  goals = read.csv(sprintf('%s/conf/goals.csv', dir_scenario), stringsAsFactors=F)
   for (g in names(goal_swap)){ # g = names(goal_swap)[1]
     for (fld in names(goal_swap[[g]])){
       goals[goals$goal==g, fld] = goal_swap[[g]][[fld]]
     }
   }
-  write.csv(goals, 'conf/goals.csv', row.names=F, na='')
+  write.csv(goals, sprintf('%s/conf/goals.csv', dir_scenario), row.names=F, na='')
   
   ## copy goals documentation ## JSL revisit: necessary?
-  file.copy(file.path(dir_github, 'ohi-webapps/subcountry2014/conf/goals.Rmd'), 'conf/goals.Rmd', overwrite=T)
+  file.copy(sprintf('%s/ohi-webapps/inst/goals.Rmd', dir_github), 
+            sprintf('%s/conf/goals.Rmd', dir_scenario), overwrite=T)
   
 }
