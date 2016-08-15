305bd5047916f4c4276b91e4941920f027d79c0d
jules32 <jstewart@nceas.ucsb.edu>
2016-08-15 14:57:28 -0700

    making clone_repo more robust

--- R/clone_repo.r
+++ R/clone_repo.r
@@ -1,18 +1,12 @@
 # clone_repo.r
 
-clone_repo <- function(dir_repos = dir_repos, 
-                       dir_repo  = dir_repo,
-                       git_url   = git_url) {
-  
-  wd = getwd()
+clone_repo <- function(dir_repos, dir_repo, git_url) {
   
   ## clone repo
-  setwd(dir_repos)
   unlink(dir_repo, recursive=T, force=T)
   repo = clone(git_url, normalizePath(dir_repo, mustWork=F))
-  setwd(dir_repo)
   
   ## get remote branches
-  remote_branches = sapply(branches(repo, 'remote'), function(x) str_split(x@name, '/')[[1]][2])
+  remote_branches = sapply(git2r::branches(repo, 'remote'), function(x) str_split(x@name, '/')[[1]][2])
   
 }
