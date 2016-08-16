1cf97892693a6a469a60ff47e66ecdf044764994
jules32 <jstewart@nceas.ucsb.edu>
2016-08-16 14:24:01 -0700

    in progress: cleaning up populate_layers.r

--- R/populate_layers.r
+++ R/populate_layers.r
@@ -1,17 +1,10 @@
 # populate_layers.r
 
-populate_layers <- function(key=key, lyrs_gl=lyrs_gl, default_scenario=default_scenario, 
-                            sfx_global=sfx_global, 
-                            multi_nation=multi_nation){
+populate_layers <- function(key, dir_repo, lyrs_gl, dir_scenario, 
+                            sfx_global = 'gl2016', multi_nation = FALSE){
   
-  ## clone repo and remotes
-  clone_repo()  
-  
-  ## create layers folder
-  dir.create(file.path(dir_repo, default_scenario, 'layers'))
-  
-  ## copy layers.csv from global ----
-  write.csv(lyrs_gl, sprintf('%s/tmp/layers_%s.csv', default_scenario, sfx_global), 
+  ## copy layers.csv from global to tmp/ ----
+  write.csv(lyrs_gl, sprintf('%s/tmp/layers_%s.csv', dir_scenario, sfx_global),   ### long term don't know if we need this tmp copy
             na='', row.names=F)
   
   ## modify tmp/layers.csv -- RETHINK THIS!! JSL July 2016
@@ -23,34 +16,17 @@ populate_layers <- function(key=key, lyrs_gl=lyrs_gl, default_scenario=default_s
     mutate(
       layer_gl = layer,
       path_in  = file.path(dir_global, 'layers', filename),
-      rgns_in  = 'global',
+      rgns_in  = 'global',                                      ## TODO long term: don't think we need this rgns_in field
       filename = sprintf('%s_%s.csv', layer, sfx_global)) %>%
     arrange(targets, layer)
   
-  ## swap out custom mar_coastalpopn_inland25mi for mar_coastalpopn_inland25km (NOTE: mi -> km)
-  ix = which(lyrs_sc$layer=='mar_coastalpopn_inland25mi')
-  lyrs_sc$layer[ix]       = 'mar_coastalpopn_inland25km'
-  lyrs_sc$path_in[ix]     = file.path(dir_annex, key, 'layers', 'mar_coastalpopn_inland25km_lyr.csv')
-  lyrs_sc$name[ix]        = str_replace(lyrs_sc$name[ix]       , fixed('miles'), 'kilometers')
-  lyrs_sc$description[ix] = str_replace(lyrs_sc$description[ix], fixed('miles'), 'kilometers')
-  lyrs_sc$filename[ix]    = 'mar_coastalpopn_inland25km_sc2014-raster.csv'
-  lyrs_sc$rgns_in[ix]     = 'subcountry'
-  
-  # swap out spatial area layers
-  area_layers = c('rgn_area'= 'rgn_offshore_data.csv')
-  
-  for (lyr in names(area_layers)){
-    csv = area_layers[lyr]
-    ix = which(lyrs_sc$layer==lyr)
-    lyrs_sc$rgns_in[ix]     = 'subcountry'
-    lyrs_sc$path_in[ix]     = file.path(dir_annex_sc, 'spatial', csv)
-    lyrs_sc$filename[ix]    = str_replace(lyrs_sc$filename[ix], fixed('_gl2014.csv'), '_sc2014-area.csv')
-  }
-  
-  TODO: 
-  - PASTE rgn_labels somewhere more useful
-  - change subcountry2014!
-    - copy_webapps_templates.r
+  ## swap out rgn_area layer
+  lyr <- 'rgn_area'
+  csv <- 'rgn_offshore_data.csv'
+  ix  <- which(lyrs_sc$layer == lyr)
+  lyrs_sc$rgns_in[ix]  <-  'subcountry'
+  lyrs_sc$path_in[ix]  <-  file.path(dir_annex_sc, 'spatial', csv)
+  lyrs_sc$filename[ix] <-  sprintf('%s.csv', lyr)
   
   ## drop cntry_* layers
   lyrs_sc = filter(lyrs_sc, !grepl('^cntry_', layer))
@@ -97,6 +73,8 @@ populate_layers <- function(key=key, lyrs_gl=lyrs_gl, default_scenario=default_s
     select(cntry_key = gl_cntry_key, sc_rgn_id) %>%
     as.data.frame()
   
+  dir.create(sprintf('%s/layers', dir_scenario), showWarnings=F)
+  
   if (!multi_nation) {
     
     ## old global to new custom countries
@@ -111,6 +89,9 @@ populate_layers <- function(key=key, lyrs_gl=lyrs_gl, default_scenario=default_s
       
       ## call copy_layer and write to layer to csv
       d <- copy_layer(lyr, sc_cntry,
+                      dir_global,
+                      sfx_global,
+                      lyrs_sc,
                       global_rgn_id = unique(sc_rgns$gl_rgn_id),
                       write_to_csv  = TRUE)
     }
@@ -167,8 +148,8 @@ populate_layers <- function(key=key, lyrs_gl=lyrs_gl, default_scenario=default_s
       clip_n_ship_disag_description,
       layer_gl,
       path_in)
-  write.csv(lyrs_reg, 'layers.csv', row.names=F, na='')
-
+  write.csv(lyrs_reg, sprintf('%s/layers.csv', dir_scenario), row.names=F, na='')
+  
   ## check for empty layers
   CheckLayers('layers.csv', 'layers',
               flds_id=c('rgn_id','country_id','saup_id','fao_id','fao_saup_id')) ##TODO: check if necessary
@@ -178,12 +159,12 @@ populate_layers <- function(key=key, lyrs_gl=lyrs_gl, default_scenario=default_s
     dir.create('tmp/layers-empty_global-values', showWarnings=F)
     write.csv(lyrs_empty, 'layers-empty_swapping-global-mean.csv', row.names=F, na='')
   }
-
+  
   ## populate empty layers with global averages. ## TODO see if a better way...
   for (lyr in subset(lyrs, data_na, layer, drop=T)){ # lyr = subset(lyrs, data_na, layer, drop=T)[1]
-
+    
     message(' for empty layer ', lyr, ', getting global avg')
-
+    
     ## get all global data for layer
     l = subset(lyrs, layer==lyr)
     csv_gl  = as.character(l$path_in)
@@ -191,16 +172,16 @@ populate_layers <- function(key=key, lyrs_gl=lyrs_gl, default_scenario=default_s
     csv_out = sprintf('layers/%s', l$filename)
     file.copy(csv_gl, csv_tmp, overwrite=T)
     a = read.csv(csv_tmp)
-
+    
     ## calculate global categorical means using non-standard evaluation, ie dplyr::*_()
     fld_key         = names(a)[1]
     fld_value       = names(a)[ncol(a)]
     flds_other = setdiff(names(a), c(fld_key, fld_value))
-
+    
     if (class(a[[fld_value]]) %in% c('factor','character') & l$fld_val_num == fld_value){
       cat(sprintf('  DOH! For empty layer "%s" field "%s" is factor/character but registered as [fld_val_num] not [fld_val_chr].\n', lyr, fld_value))
     }
-
+    
     ## exceptions
     if (lyr == 'mar_trend_years'){
       sc_rgns %>%
@@ -208,14 +189,14 @@ populate_layers <- function(key=key, lyrs_gl=lyrs_gl, default_scenario=default_s
         select(rgn_id = sc_rgn_id, trend_yrs) %>%
         arrange(rgn_id) %>%
         write.csv(csv_out, row.names=F, na='')
-
+      
       next
     }
-
+    
     if (class(a[[fld_value]]) %in% c('factor','character')){
       cat(sprintf('  DOH! For empty layer "%s" field "%s" is factor/character but continuing with presumption of numeric.\n', lyr, fld_value))
     }
-
+    
     ## presuming numeric...
     ## get mean
     #if (lyr == 'mar_coastalpopn_inland25km') browser()
@@ -233,7 +214,7 @@ populate_layers <- function(key=key, lyrs_gl=lyrs_gl, default_scenario=default_s
             sprintf('mean(%s, na.rm=T)', fld_value),
             fld_value))
     }
-
+    
     ## bind many rgn_ids
     if ('rgn_id' %in% names(a) | 'cntry_key' %in% names(a)){
       b = b %>%
@@ -244,33 +225,22 @@ populate_layers <- function(key=key, lyrs_gl=lyrs_gl, default_scenario=default_s
         select(one_of('rgn_id', flds_other, fld_value)) %>%
         arrange(rgn_id)
     }
-
+    
     #if (lyr == 'mar_harvest_tonnes') browser()
     write.csv(b, csv_out, row.names=F, na='')
-
+    
   } # end for (lyr in subset(lyrs, data_na, layer, drop=T))
-
+  
   ## update layers.csv with empty layers now populated by global averages
   CheckLayers('layers.csv', 'layers',
               flds_id=c('rgn_id','country_id','saup_id')) ##,'fao_id','fao_saup_id'))
-
-  ## someday fix these warnings that happened with COL July 2016
-# Warning messages:
-# 1: In CheckLayers("layers.csv", "layers", flds_id = c("rgn_id", "country_id",  :
-# Missing files...these files are not found in the layers folder
-# rgn_labels: rgn_labels_gl2016.csv
-# 2: In CheckLayers("layers.csv", "layers", flds_id = c("rgn_id", "country_id",  :
-# Unused fields...
-# fis_b_bmsy: taxon_name
-# fis_meancatch: taxon_name_key
-# rgn_area: rgn_name
-# 3: In CheckLayers("layers.csv", "layers", flds_id = c("rgn_id", "country_id",  :
-# Rows duplicated...
-# fis_b_bmsy: 69951
-# fis_meancatch: 108700
-# ico_spp_extinction_status: 9
-# ico_spp_popn_trend: 9
-
+  
+  ## TODO: copy rgn_area layer into /spatial
+  
+  
+  # TODO: 
+  # - change subcountry2014!
+  
 }
 
 # calculate_scores.r, install_ohicore
