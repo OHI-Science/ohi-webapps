5eef78b0b04d8045e540c15625b8f3988ce039bd
jules32 <jstewart@nceas.ucsb.edu>
2016-09-07 15:09:51 -0700

    migrate functions from ohi-functions.r to /R

--- ohi-functions.r
+++ ohi-functions.r
@@ -1,44 +1,46 @@
 # copy of ohi-travis-functions.r, without the Travis requirements
-calculate_scores_notravis <- function(){ # to distinguish from assessmt/scenario/calculate_scores.r
-  
-  # load required libraries
-  wd = getwd()
-  suppressWarnings(require(ohicore))
-  
-  # ensure on draft repo
-  library(git2r)
-  repo = repository(wd)
-  checkout(repo, 'draft')
-  
-  # iterate through all scenarios (by finding layers.csv)
-  dirs_scenario = normalizePath(dirname(list.files('.', '^layers.csv$', recursive=T, full.names=T)))
-  for (dir_scenario in dirs_scenario){ # dir_scenario=dirs_scenario[1]
-    
-    # set working directory to scenario
-    setwd(dir_scenario)
-    cat('\n\nCALCULATE SCORES for SCENARIO', basename(dir_scenario), '\n')
-    
-    # load scenario configuration
-    conf <<- Conf('conf')
-    
-    # run checks on scenario layers
-    CheckLayers('layers.csv', 'layers', flds_id=conf$config$layers_id_fields)
-    
-    # load scenario layers
-    layers <<- Layers('layers.csv', 'layers')
-    
-    # calculate scenario scores
-    scores = CalculateAll(conf, layers, debug=F)
-    write.csv(scores, 'scores.csv', na='', row.names=F)
-    
-    # document versions of packages and specifics of ohicore
-    cat(
-      capture.output(sessionInfo()), '\n\n',
-      readLines(file.path(system.file(package='ohicore'), 'DESCRIPTION')),
-      file='session.txt', sep='\n')
-  }
-  setwd(wd)
-}
+
+## moved to R/calculate_scores_check()
+# calculate_scores_notravis <- function(){ # to distinguish from assessmt/scenario/calculate_scores.r
+#   
+#   # load required libraries
+#   wd = getwd()
+#   suppressWarnings(require(ohicore))
+#   
+#   # ensure on draft repo
+#   library(git2r)
+#   repo = repository(wd)
+#   checkout(repo, 'draft')
+#   
+#   # iterate through all scenarios (by finding layers.csv)
+#   dirs_scenario = normalizePath(dirname(list.files('.', '^layers.csv$', recursive=T, full.names=T)))
+#   for (dir_scenario in dirs_scenario){ # dir_scenario=dirs_scenario[1]
+#     
+#     # set working directory to scenario
+#     setwd(dir_scenario)
+#     cat('\n\nCALCULATE SCORES for SCENARIO', basename(dir_scenario), '\n')
+#     
+#     # load scenario configuration
+#     conf <<- Conf('conf')
+#     
+#     # run checks on scenario layers
+#     CheckLayers('layers.csv', 'layers', flds_id=conf$config$layers_id_fields)
+#     
+#     # load scenario layers
+#     layers <<- Layers('layers.csv', 'layers')
+#     
+#     # calculate scenario scores
+#     scores = CalculateAll(conf, layers, debug=F)
+#     write.csv(scores, 'scores.csv', na='', row.names=F)
+#     
+#     # document versions of packages and specifics of ohicore
+#     cat(
+#       capture.output(sessionInfo()), '\n\n',
+#       readLines(file.path(system.file(package='ohicore'), 'DESCRIPTION')),
+#       file='session.txt', sep='\n')
+#   }
+#   setwd(wd)
+# }
 
 update_results <- function(){
   
@@ -402,10 +404,11 @@ update_pages <- function(){
   setwd(wd)
 }
 
-push_branch <- function(branch='draft', current_msg='pushing draft branch no travis'){ 
-  
-  # working locally, gh_token set in create_init.R, repo_name set in create_init_sc.Rs
-  system(sprintf('git add -A; git commit -a -m " %s" ', current_msg))
-  system(sprintf('git push https://%s@github.com/%s.git HEAD:%s', gh_token, git_slug, branch))
-  
-}
+## JSL moved to R/push_branch
+# push_branch <- function(branch='draft', current_msg='pushing draft branch no travis'){ 
+#   
+#   # working locally, gh_token set in create_init.R, repo_name set in create_init_sc.Rs
+#   system(sprintf('git add -A; git commit -a -m " %s" ', current_msg))
+#   system(sprintf('git push https://%s@github.com/%s.git HEAD:%s', gh_token, git_slug, branch))
+#   
+# }
