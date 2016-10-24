84073324c6aeba5b9f1481d50cceeaf9fdb980d1
Jamie Afflerbach <afflerbach@nceas.ucsb.edu>
2016-10-24 10:23:43 -0700

    init commit

---
+++ custom/arc/arctic_land.R
@@ -0,0 +1,24 @@
+## Getting Arctic Land for Mike
+
+# Jamie Afflerbach
+
+dir_M             <- c('Windows' = '//mazu.nceas.ucsb.edu/ohi',
+                       'Darwin'  = '/Volumes/ohi',    ### connect (cmd-K) to smb://mazu/ohi
+                       'Linux'   = '/home/shares/ohi')[[ Sys.info()[['sysname']] ]]
+
+library(rgdal)
+library(dplyr)
+
+laeaCRS <- CRS("+init=epsg:3572")
+
+global_shp <- readOGR(dsn = file.path(dir_M,'git-annex/globalprep/spatial/d2014/data'), layer = 'regions_gcs')
+
+arctic_land <- global_shp%>%
+                subset(rgn_nam %in% c("Russia","Canada","Norway", "United States", "Jan Mayen", "Greenland"))%>%
+                subset(rgn_typ %in% c("land", "land-disputed"))
+
+arctic_land_laea <- spTransform(arctic_land, laeaCRS)
+
+plot(arctic_land_laea)
+
+writeOGR(arctic_land_laea,dsn = "globalprep/spatial",layer = "arctic_layer", driver = "ESRI Shapefile")
