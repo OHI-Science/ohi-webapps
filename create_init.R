17d675dfeb3e3297e4d3ed6691e95be059804031
jules32 <jstewart@nceas.ucsb.edu>
2017-07-07 10:09:39 -0700

    rename sc_studies_custom -> repo_registry, clean up columns

--- create_init.R
+++ create_init.R
@@ -1,20 +1,15 @@
 # create_init.R
 # by @bbest and @jules32. load all libraries, set directories relevant to all keys
 
-## NOTE: eventually add these to ohirepos and replace w/ ohirepos
-library(ohirepos) # devtools::install_github('ohi-science/ohirepos')
-library(tidyverse)
-library(stringr) ## should be in tidyverse
-library(git2r)     # install.packages('git2r')
+## NOTE: moved all but thes to ohirepos, do we need them? 
 library(rgdal)
 library(jsonlite)
-library(brew)
-library(yaml)
-merge <- base::merge # override git2r::merge
-select <- dplyr::select
 
-# set working dir
-setwd('~/github/ohi-webapps') 
+# merge <- base::merge # override git2r::merge
+# select <- dplyr::select
+
+# # set working dir
+# setwd('~/github/ohi-webapps') 
 
 # vars
 # get paths based on host machine
@@ -72,42 +67,5 @@ gl_rgns <- read.csv(csv_gl_rgn, stringsAsFactors=F) %>%
   arrange(gl_rgn_key, gl_rgn_name)
 stopifnot(count(gl_rgns, gl_rgn_key) %>% filter(n > 1) %>% nrow == 0)
 
-# sc_annex_dirs <- list.dirs(dir_annex, full.names=F, recursive=F)
-# sc_studies <- gl_rgns %>%
-#   mutate(
-#     sc_key  = tolower(gl_rgn_key),
-#     sc_name = gl_rgn_name)  %>%
-#   left_join(
-#     data.frame(
-#       sc_key       = sc_annex_dirs,
-#       sc_annex_dir = file.path(dir_annex, sc_annex_dirs)),
-#     by = 'sc_key') %>%
-#   mutate(
-#     sc_key_old = tolower(str_replace_all(sc_name, ' ', '_')),
-#     sc_key     = tolower(gl_rgn_key)) %>%
-#   select(sc_key, sc_name, sc_key_old, gl_rgn_id, gl_rgn_name, gl_rgn_key, sc_annex_dir) %>%
-#   arrange(sc_key)
-
-# read in custom sc studies
-sc_studies = read.csv('custom/sc_studies_custom.csv', stringsAsFactors=F)
-# sc_studies = sc_studies %>%
-#   anti_join(sc_custom, by = c('sc_key', 'sc_name', 'gl_rgn_id')) %>%  # removes original chn, col
-#     rbind(                                                            # rbinds custom chn, col
-#   sc_custom[, names(sc_studies)])
-# stopifnot(count(sc_studies, sc_key) %>% filter(n > 1) %>% nrow == 0)
-
-# report on subcountries without annex data dirs
-# if (nrow(filter(sc_studies, is.na(sc_annex_dir))) > 0){
-#   message(
-#     sprintf(
-#       'Looking for prepped data folders by subcountry key (lowercase gl_rgn_key):\n    %s\n  The following global regions were not found:\n    %s',
-#       dir_annex, paste(with(filter(sc_studies, is.na(sc_annex_dir)), sprintf('%s (%s)', gl_rgn_name, sc_key)), collapse = '\n    ')))
-# }
-
-# write.csv(
-#   sc_studies %>%
-#     filter(is.na(sc_annex_dir)) %>%
-#     select(gl_rgn_id, gl_rgn_name, sc_key) %>%
-#     arrange(gl_rgn_name), 'tmp/gl-rgns_no-sc-annex-data.csv', row.names=F, na='')
-# #sc_studies = filter(sc_studies, !is.na(gl_rgn_key))
-# stopifnot(count(sc_studies, sc_key) %>% filter(n > 1) %>% nrow == 0)
+# read in repo_registry
+repo_registry = readr::read_csv('repo_registry.csv')
--- create_init_sc.R
+++ create_init_sc.R
@@ -2,11 +2,11 @@
 # by @bbest. # load all variables, set directories specific to individual key
 
 stopifnot(exists('key'))
-i             = which(sc_studies$sc_key==key)
-sc            = sc_studies[i,]
-name          = sc$sc_name
+i             = which(repo_registry$key==key)
+sc            = repo_registry[i,]
+name          = sc$study_area
 study_area    = name
-name_gl_rgn       = sc$sc_key_old
+# name_gl_rgn       = sc$sc_key_old TO DELETE
 name_gl_rgn_cap   = sc$gl_rgn_name
 gl_rgn_id         = sc$gl_rgn_id
 repo_name     = key
@@ -16,8 +16,8 @@ git_slug      = sprintf('%s/%s', git_owner, git_repo)
 git_url       = sprintf('https://github.com/%s', git_slug)
 pages_url     = sprintf('http://ohi-science.org/%s', git_repo)
 dir_repo      = file.path(dir_repos, repo_name)
-if (key %in% sc_studies$sc_key){
-  default_scenario = subset(sc_studies, sc_key==key, default_scenario, drop=T)
+if (key %in% repo_registry$key){
+  default_scenario = subset(repo_registry, key==key, default_scenario, drop=T)
 } else {
   message('Please enter a scenario name in `custom/sc_studies_custom.csv') # no longer default to 'subcountry2014'
 }
@@ -37,9 +37,9 @@ dir_annex_sc  = file.path(dir_annex, key)
 app_name      = key # sprintf('%s_app', key)
 csv_pop_inland25km = file.path(dir_M, 'git-annex/clip-n-ship', key, 'layers/mar_coastalpopn_inland25km_lyr.csv') # TODO: sort out mar_pop_inland stuff
 
-study_area = subset(sc_studies, sc_key==key, sc_name, drop=T)
+study_area = subset(repo_registry, key==key, study_area, drop=T)
 
-#message(sprintf('\n%03d of %d: %s [%s] -- %s', i, nrow(sc_studies), name, key, format(Sys.time(), '%X')))    
+#message(sprintf('\n%03d of %d: %s [%s] -- %s', i, nrow(repo_registry), name, key, format(Sys.time(), '%X')))    
 stopifnot(!is.na(key))
 
 
