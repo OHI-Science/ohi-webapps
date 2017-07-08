41f06f42d3534176c4ec5dcd811fa696677be96d
jules32 <jstewart@nceas.ucsb.edu>
2017-07-07 17:34:34 -0700

    with ohirepos, replaced create_init with common.r

--- common.r
+++ common.r
@@ -24,46 +24,32 @@ github_user <- c('bbest'='bbest','julialowndes'='jules32')[Sys.info()["user"]]
 dir_sandbox <- '~/github/clip-n-ship'
 dir_origin  <- '~/github/ohi-global/eez2016'
 
-# dw_year     <- 2014 # downweight year for mar_coastalpopn_inland25km_lyr on Mazu # TODO DELETE?
-git_branch  <- 'master'
+## TODO delete once tidy with Mel July 2017
+# fxn_swap    <- c(
+#   'LIV_ECO' = file.path(dir_github, 'ohi-webapps/functions/functions_LIV_ECO.R'),
+#   'LE'      = file.path(dir_github, 'ohi-webapps/functions/functions_LE.R'), 
+#   'ICO'     = file.path(dir_github, 'ohi-webapps/functions/functions_ICO.R'))
+# goal_swap   <- list(
+#   'LIV' = list(preindex_function="LIV_ECO(layers, subgoal='LIV')"),
+#   'ECO' = list(preindex_function="LIV_ECO(layers, subgoal='ECO')"))
 
-fxn_swap    <- c(
-  'LIV_ECO' = file.path(dir_github, 'ohi-webapps/functions/functions_LIV_ECO.R'),
-  'LE'      = file.path(dir_github, 'ohi-webapps/functions/functions_LE.R'), 
-  'ICO'     = file.path(dir_github, 'ohi-webapps/functions/functions_ICO.R'))
-goal_swap   <- list(
-  'LIV' = list(preindex_function="LIV_ECO(layers, subgoal='LIV')"),
-  'ECO' = list(preindex_function="LIV_ECO(layers, subgoal='ECO')"))
-
-
-# read global layers, add clip_n_ship columns from Master layers doc (originally layers_global on Google docs)
-lyrs_gl     <- read.csv(file.path(dir_global, 'layers.csv'), stringsAsFactors = F)
-# lyrs_google <- read.csv(file.path('~/github/ohi-global', 'layers_eez.csv'), stringsAsFactors = F)
-# lyrs_gl <- lyrs_gl %>%
-#   left_join(
-#     lyrs_google # %>% select(layer, starts_with('clip_n_ship'))
-#     , by='layer')
 
 # read in github token outside of repo, generated via https://help.github.com/articles/creating-an-access-token-for-command-line-use
 gh_token <- scan('~/.github-token', 'character', quiet = T)
 Sys.setenv(GH_TOKEN=gh_token)
 
-gl_cntries <- read.csv(sprintf('%s/layers/cntry_rgn.csv', dir_global)) %>%
-  select(
-    gl_cntry_key = cntry_key,
-    gl_rgn_id    = rgn_id) %>%
-  arrange(gl_cntry_key)
-
-csv_gl_rgn  <- 'sp_rgn_manual_v2014.csv' # for rgn_key # TODO JSL see if this is necessary Dec 2016
-gl_rgns <- read.csv(csv_gl_rgn, stringsAsFactors=F) %>%
-  dplyr::distinct(rgn_id, rgn_name, rgn_key, .keep_all = TRUE) %>%
-  filter(rgn_type == 'eez') %>%
-  select(
-    gl_rgn_id   = rgn_id,
-    gl_rgn_name = rgn_name,
-    gl_rgn_key  = rgn_key) %>%
-  arrange(gl_rgn_key, gl_rgn_name)
-stopifnot(count(gl_rgns, gl_rgn_key) %>% filter(n > 1) %>% nrow == 0)
+## TODO delete: may have something to do with LE fxn_swap
+# csv_gl_rgn  <- 'sp_rgn_manual_v2014.csv' # for rgn_key # TODO JSL see if this is necessary Dec 2016
+## TODO delete this var July 7 2017
+# gl_rgns <- read.csv(csv_gl_rgn, stringsAsFactors=F) %>%
+#   dplyr::distinct(rgn_id, rgn_name, rgn_key, .keep_all = TRUE) %>%
+#   filter(rgn_type == 'eez') %>%
+#   select(
+#     gl_rgn_id   = rgn_id,
+#     gl_rgn_name = rgn_name,
+#     gl_rgn_key  = rgn_key) %>%
+#   arrange(gl_rgn_key, gl_rgn_name)
+# stopifnot(count(gl_rgns, gl_rgn_key) %>% filter(n > 1) %>% nrow == 0)
 
 # read in repo_registry
 repo_registry = readr::read_csv('repo_registry.csv')
